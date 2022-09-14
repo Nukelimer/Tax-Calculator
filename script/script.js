200cc9ae15402cbdc038b3d0a45dfb595196978d
codewithremi <codewithremi@gmail.com>
2022-09-14 08:08:01 +0100

    Changes...Restructure.

--- script/script.js
+++ script/script.js
@@ -1,59 +1,54 @@
 const billTotalInput = document.getElementById("billTotalInput");
-const tipInput = document.getElementById("tipInput");
+const vatInput = document.getElementById("tipInput");
 let numberOfPeople = document.getElementById("numberOfPeople");
 const perPersonTotal = document.getElementById("perPersonTotal");
+const Total = document.getElementById("totalVAT");
+const vatTotal = document.getElementById("perPersonTotal");
+const calculateBills = document.querySelector(".calculateBill");
+const calculateVATs = document.querySelector(".calculateVat");
+const increase = document.querySelector(".increasePeople");
+const decrease = document.querySelector(".decreasePeople");
 
 let numberOfPerson = Number(numberOfPeople.innerText);
 
 // ** Calculate the total bill per person **
 const calculateBill = () => {
   const bill = Number(billTotalInput.value);
-  const tips = Number(tipInput.value) / 100;
-  const totalTipMultiplied = tips * bill;
+  const vat = Number(tipInput.value) / 100;
+  const totalVatMultiplied = vat * bill;
 
-  const totaltipAdded = bill + totalTipMultiplied;
+  const totalVatAdded = bill + totalVatMultiplied;
 
-  const perPersonTotalAmount = totaltipAdded / numberOfPerson;
+  const perPersonTotalAmount = totalVatAdded / numberOfPerson;
 
-  perPersonTotal.textContent = `$${perPersonTotalAmount.toFixed(2)}`;
+  vatTotal.textContent = `\u20A6 ${perPersonTotalAmount.toLocaleString(
+    "us-EN"
+  )}`;
 
-  // get bill from user input & convert it into a number
-  // get the tip from user & convert it into a percentage (divide by 100)
-  // get the total tip amount
-  // calculate the total (tip amount + bill)
-  // calculate the per person total (total divided by number of people)
-  // update the perPersonTotal on DOM & show it to user
+  Total.textContent = `\u20A6 ${totalVatMultiplied.toLocaleString("us-EN")}`;
 };
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
-  numberOfPerson++
+  numberOfPerson++;
   numberOfPeople.textContent = numberOfPerson;
-  
-  calculateBill()
-  // increment the amount of people
-  // update the DOM with the new number of people
-  // calculate the bill based on the new number of people
+
+  calculateBill();
 };
 
 // ** Splits the bill between fewer people **
 const decreasePeople = () => {
   if (numberOfPerson <= 1) {
- numberOfPeople.textContent = "Tap +"
+    numberOfPeople.textContent = "Tap +";
     return;
-
-  }
-  else {
-    numberOfPerson--
+  } else {
+    numberOfPerson--;
     numberOfPeople.textContent = numberOfPerson;
-    calculateBill()
+    calculateBill();
   }
- 
+};
 
- 
-//     numberOfPeople.innerText--
-//   // decrement the amount of people
-//   // update the DOM with the new number of people
-//   // calculate the bill based on the new number of people
-  // }
-}
+decrease.addEventListener("click", decreasePeople);
+increase.addEventListener("click", increasePeople);
+calculateBills.addEventListener("keyup", calculateBill);
+calculateVATs.addEventListener("keyup", calculateBill);
